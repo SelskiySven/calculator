475b312bd01ce1b56d1508294a66f73e5865ead8
CountryPig <72417764+SelskiySven@users.noreply.github.com>
2022-04-12 20:49:24 +0300

    Add files via upload

---
+++ calc/Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBoxIs = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBoxIs
+        '
+        Me.TextBoxIs.Location = New System.Drawing.Point(45, 28)
+        Me.TextBoxIs.Name = "TextBoxIs"
+        Me.TextBoxIs.Size = New System.Drawing.Size(566, 23)
+        Me.TextBoxIs.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(288, 57)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 25)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "="
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(643, 98)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBoxIs)
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxIs As TextBox
+    Friend WithEvents Button1 As Button
+End Class
